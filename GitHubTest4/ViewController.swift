81b5c565b04568f374707be50f65ca4496038e48
KumiKumiKUmi <g12906hk+git@gmail.com>
2015-05-04 22:57:29 +0900

    test3の表示

--- GitHubTest4/ViewController.swift
+++ GitHubTest4/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         println("test1")
         println("test2")
+        println("test3") //
     }
 
     override func didReceiveMemoryWarning() {
