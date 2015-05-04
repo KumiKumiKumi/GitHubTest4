f7e17fd3210cd7ff8566b4cbdb11c2a5e97dcc13
ErikaTai <g12951te@gm.tsuda.ac.jp>
2015-05-05 00:22:16 +0900

    test3を削除

--- GitHubTest4/ViewController.swift
+++ GitHubTest4/ViewController.swift
@@ -15,7 +15,6 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         println("test1")
         println("test2")
-        println("test3") //
     }
 
     override func didReceiveMemoryWarning() {
